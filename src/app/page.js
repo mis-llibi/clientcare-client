a8f6af15c7672b0bada93c35763acc59998811eb
0ojireho0 <jeremiahquintano17@gmail.com>
2025-09-25 13:28:22 +0800

    fix env

--- src/app/page.js
+++ src/app/page.js
@@ -4,6 +4,7 @@ export default function Home() {
   return (
     <div className="text-center">
       <h1 className="font-bold roboto">Request a LOA in your preferred hospital</h1>
+      {process.env.NEXT_PUBLIC_DEPLOYED_BACKEND_API}
     </div>
   );
 }
